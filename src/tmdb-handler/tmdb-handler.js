1bdfd2d97b3917f089c6f0aebc520a4684181fa4
Tamas Dinh <tamas.dinh@gmail.com>
2022-02-20 17:59:06 +0100

    fix: API key transfer issue

--- src/tmdb-handler/tmdb-handler.js
+++ src/tmdb-handler/tmdb-handler.js
@@ -1,14 +1,16 @@
+const API_KEY = 'f033c108e832c39fe7a4a8722ea80a52'
+
 export class TmdbHandler {
 
     static async getPopular(page = 1) {
         const url = 'https://api.themoviedb.org/3/movie/popular'
-        const response = await fetch(`${url}?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
+        const response = await fetch(`${url}?api_key=${API_KEY}&page=${page}`)
         return await response.json()
     }
 
     static async getDetails(id) {
         const url = `https://api.themoviedb.org/3/movie/${id}`
-        const response = await fetch(`${url}?api_key=${process.env.REACT_APP_API_KEY}`)
+        const response = await fetch(`${url}?api_key=${API_KEY}`)
         return response.json()
     }
 
