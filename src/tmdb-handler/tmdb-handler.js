8835cb33e9847298d348a5e413df103fc08818c4
Tamas Dinh <tamas.dinh@gmail.com>
2022-02-20 17:29:56 +0100

    fix: API key reference

--- src/tmdb-handler/tmdb-handler.js
+++ src/tmdb-handler/tmdb-handler.js
@@ -1,16 +1,14 @@
-const API_KEY = process.env.REACT_APP_API_KEY
-
 export class TmdbHandler {
 
     static async getPopular(page = 1) {
         const url = 'https://api.themoviedb.org/3/movie/popular'
-        const response = await fetch(`${url}?api_key=${API_KEY}&page=${page}`)
+        const response = await fetch(`${url}?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`)
         return await response.json()
     }
 
     static async getDetails(id) {
         const url = `https://api.themoviedb.org/3/movie/${id}`
-        const response = await fetch(`${url}?api_key=${API_KEY}`)
+        const response = await fetch(`${url}?api_key=${process.env.REACT_APP_API_KEY}`)
         return response.json()
     }
 
